1e393c802946c563302dcca4963d195a34077bd9
Georgios Karamoussanlis <georgios.karamoussanlis@tu-dortmund.de>
2025-04-28 11:32:13 +0200

    neue kleinere graphen für zoo runs, weil die >100 runs vorraussichtlich 2 Wochen brauchen

--- run_all_zoo.sh
+++ run_all_zoo.sh
@@ -2,14 +2,14 @@
 
 # Cluster-Experimente
 python3 planar_experimentsRANDOM_ZOO_57.py zoo 123 1 20 18 CLUSTER &
-python3 planar_experimentsRANDOM_ZOO_102.py zoo 123 1 20 18 CLUSTER &
-python3 planar_experimentsRANDOM_ZOO_136.py zoo 123 1 20 18 CLUSTER &
+python3 planar_experimentsRANDOM_ZOO_54.py zoo 123 1 20 18 CLUSTER &
+python3 planar_experimentsRANDOM_ZOO_49.py zoo 123 1 20 18 CLUSTER &
 
 
 # Random-Experimente
 python3 planar_experimentsRANDOM_ZOO_57.py zoo 123 1 20 18 RANDOM &
-python3 planar_experimentsRANDOM_ZOO_102.py zoo 123 1 20 18 RANDOM &
-python3 planar_experimentsRANDOM_ZOO_136.py zoo 123 1 20 18 RANDOM &
+python3 planar_experimentsRANDOM_ZOO_54.py zoo 123 1 20 18 RANDOM &
+python3 planar_experimentsRANDOM_ZOO_49.py zoo 123 1 20 18 RANDOM &
 
 # Auf alle Prozesse warten
 wait
