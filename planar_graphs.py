f8f56e651e550d21e130d5bb88d1ae654ff3b36f
Georgios Karamoussanlis <georgios.karamoussanlis@tu-dortmund.de>
2025-05-26 14:35:10 +0200

    kommentare aufräumen

--- planar_graphs.py
+++ planar_graphs.py
@@ -3,8 +3,6 @@ import networkx as nx
 from scipy.spatial import Delaunay
 import math
 
-#from one_tree_experiments import shuffle_and_run
-
 def create_unit_disk_graph(num_nodes, initial_radius=0.1):
     """
     Erzeugt einen zusammenhängenden Unit-Disk-Graph mit Positionsattributen.
@@ -13,14 +11,12 @@ def create_unit_disk_graph(num_nodes, initial_radius=0.1):
     max_attempts = 100
     attempt = 0
     while attempt < max_attempts:
-        # Erzeuge zufällige Positionen mit der Standard-Random-Bibliothek
         initial_positions = {i: (random.random(), random.random()) for i in range(num_nodes)}
         G = nx.Graph()
         for i, pos in initial_positions.items():
             G.add_node(i, pos=pos)
         for i in range(num_nodes):
             for j in range(i + 1, num_nodes):
-                # Berechne die Distanz ohne numpy
                 dist = math.sqrt((initial_positions[i][0] - initial_positions[j][0]) ** 2 +
                                  (initial_positions[i][1] - initial_positions[j][1]) ** 2)
                 if dist <= radius:
@@ -36,14 +32,13 @@ def apply_delaunay_triangulation(G):
     Wendet die Delaunay-Triangulation auf die gespeicherten Knotenpositionen an.
     """
     positions = {i: G.nodes[i]['pos'] for i in G.nodes}
-    points = list(positions.values())  # Verwende eine Liste von Tupeln statt numpy-Array
+    points = list(positions.values()) 
     tri = Delaunay(points)
     H = nx.Graph()
     H.add_nodes_from(G.nodes(data=True))
     for simplex in tri.simplices:
         for i in range(len(simplex)):
             for j in range(i + 1, len(simplex)):
-                # Konvertiere die Indizes in Standard-Python-Integer
                 node1, node2 = int(simplex[i]), int(simplex[j])
                 H.add_edge(node1, node2)
     return H
@@ -58,7 +53,6 @@ def apply_gabriel_graph(G):
     for i in G.nodes:
         for j in G.nodes:
             if i < j:
-                # Berechne die Distanz ohne numpy
                 dist = math.sqrt((positions[i][0] - positions[j][0]) ** 2 + (positions[i][1] - positions[j][1]) ** 2)
                 midpoint = ((positions[i][0] + positions[j][0]) / 2, (positions[i][1] + positions[j][1]) / 2)
                 if dist <= 1.0 and all(
